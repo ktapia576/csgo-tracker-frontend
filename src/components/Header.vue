6efe12ea69e3186983a5596bcab1aca2ec6305ba
ktapia576 <ktapia576@hotmail.com>
2021-03-27 01:25:00 -0400

    Add Header component and add styling

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <img :src="image" class="img-fluid">
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+      return {
+          image: require('@/assets/csgo.jpg')
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
