698fcbd6fa575bdb3cb082cec071a9971bffa158
ktapia576 <ktapia576@hotmail.com>
2021-03-29 19:56:40 -0400

    Add profile component route

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Search from './components/Search.vue'
+import Profile from './components/Profile.vue'
 
 const routes = [
     {
         path: '/',
-        name: 'Search',
+        name: 'search',
         component: Search
+    },
+    {
+        path: '/profile/steam/:steamID',
+        name: 'profile',
+        component: Profile
     }
 ]
 
